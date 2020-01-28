9aa99fb1b870328bb6d3d10a0a340f441d71a267
unknown <acslook@hotmail.com>
2020-01-27 21:49:43 -0300

    improving category form template

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+
+import { Category } from "../shared/category.model";
+import { CategoryService } from "../shared/category.service";
+
+import { switchMap } from "rxjs/operators";
+
+import { toastr } from "toastr";
 
 @Component({
   selector: 'app-category-form',
@@ -7,9 +16,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryFormComponent implements OnInit {
 
-  constructor() { }
+  currentAction: string;
+  categoryForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submittingForm: boolean = false;
+  category: Category = new Category();
+
+  constructor(
+    private categoryService: CategoryService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formbuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.setCurrentAction();
+    this.buildCategory();
+    this.loadCategory();
   }
 
+  ngAfterContentChecked(){
+    this.setPageTitle();
+  }
+  
+  private setPageTitle() {
+    if(this.currentAction == "new")
+      this.pageTitle = "Cadastro de Nova Categoria";
+    else{
+      const categoryName = this.category.name || ""
+      this.pageTitle = "Editando categoria: " + categoryName;
+    }
+  }
+  
+  private setCurrentAction() {
+    if(this.route.snapshot.url[0].path == "new")
+      this.currentAction = "new";
+    else
+      this.currentAction = "edit";
+
+  }
+  
+  private buildCategory() {
+    this.categoryForm = this.formbuilder.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: [null]
+    });
+  }
+
+  private loadCategory() {
+    if(this.currentAction == "edit"){
+      this.route.paramMap.pipe(
+        switchMap(params => this.categoryService.getById(+params.get("id")))
+      )
+      .subscribe(
+        (c)=>{
+          this.category = c;
+          this.categoryForm.patchValue(c);
+        },
+        (error) => alert('Ocorreu um erro no servidor, tente novamente.')
+      )
+    }
+  }
 }
